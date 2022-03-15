bb1d7803fd8ec6b8f2cb46e579a45fdb29033ada
Zakariye Aden <zakariye.aden00@gmail.com>
2022-03-15 16:48:01 -0500

    having issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import GeneralInformation from './GeneralInfo';
+import Experience from './experience';
+import Education from './education';
 
-function App() {
-  return (
-    <div>
-      
+
+const flexStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
+export default function App(){
+  const [educationId, setEducationId] = useState([]);
+  const [experienceId, setExperienceId] = useState([]);
+
+  const handleAddClick = (e) => {
+    if(e.target.name === "addEducation"){
+      setEducationId((edu) => [...edu, Date.now()]);
+    }else{
+      setExperienceId((exp) => [...exp, Date.now()]);
+    } 
+  };
+
+  const handleDelete = (name, id) => {
+    if(name === "education"){
+      setEducationId((edu) => {
+        let newList = edu.filter((key) => key !== id);
+        return newList;
+      });
+    }else {
+      setExperienceId((exp) => {
+        let newList = exp.filter((key) => key !== id);
+        return newList;
+      });
+    }
+  };
+
+  const education = educationId.map((id) => (
+    <Education key={id} id={id} handleDelete={handleDelete} />
+  ));
+
+  const experience = experienceId.map((id) => (
+    <Experience key={id} id={id} handleDelete={handleDelete}/>
+  ));
+  return(
+    <div style={flexStyle}>
+      <h1>CV Application</h1>
+      <GeneralInformation/>
+
+        <h3>Education</h3>
+          {education}
+      <button name="addEducation" onClick={handleAddClick}>Add Education</button>
+        
+      <h3>Experience</h3>
+        {experience}
+        <button name="addExperience" onClick={handleAddClick}> Add Experience</button>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/view.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function View(props){
+  console.log(props);
+  return(
+    <div>
+      <span onClick={props.handleEdit}>Edit</span>
+    </div>
+  );
+}
